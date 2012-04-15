1c503c0a45bb76b8f6ff595acd19abf0aea6d03e
tai <zzwzzhao@163.com>
2012-04-15 09:56:10 +0800

    Added admin namespaced users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Ticketee::Application.routes.draw do
 
+
   devise_for :users
 
   root :to => "projects#index"
 
   resources :projects do
     resources :tickets
+  end
+
+  namespace :admin do
+    resources :users
   end
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/support/seed_helpers.rb
@@ -0,0 +1,11 @@
+module SeedHelpers
+  def create_user!(attributes={})
+    user = Factory(:user, attributes)
+    user.confirm!
+    user
+  end
+
+  RSpec.configure do |config|
+    config.include SeedHelpers
+  end
+end
