81a96712a4808feccc0b21ee3da24ef60c3a1407
tai <zzwzzhao@163.com>
2012-04-16 23:36:20 +0800

    Users can now leave comments on tickets

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Ticketee::Application.routes.draw do
     resources :tickets
   end
 
+  resources :tickets do
+    resources :comments
+  end
+
   namespace :admin do
     root :to => "base#index"
     resources :users do
